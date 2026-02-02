c39b6c7e1cda13ae455feb4910909b0d2e3880d3
Bousalih Hamza <bousalih.hamza.bsh@gmail.com>
2026-02-02 20:18:36 +0100

    update docker image to use new account and add "Check status" to installation steps

--- app/layout.jsx
+++ app/layout.jsx
@@ -71,7 +71,7 @@ export default async function RootLayout({ children }) {
           {children}
         </Layout>
         <script data-collect-dnt="true" async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
-        <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif?collect-dnt=true" alt="" referrerpolicy="no-referrer-when-downgrade" /></noscript>
+        <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif?collect-dnt=true" alt="" referrerPolicy="no-referrer-when-downgrade" /></noscript>
       </body>
     </html>
   )
